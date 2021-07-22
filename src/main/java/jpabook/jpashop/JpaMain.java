1cf2cf81f3e27129d1b8933bcb6e9f8890346604
joelyou-nsuslab <joel.you@nsuslab.com>
2021-07-22 16:50:29 +0900

    Jpa Study 7 Days ..

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop;
 
 import jpabook.jpashop.domain.Book;
+import jpabook.jpashop.domain.Item;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -24,6 +25,8 @@ public class JpaMain {
             book.setAuthor("김영한");
 
             em.persist(book);
+
+            em.createQuery("select i from Item i where type(i) = Book", Item.class);
 
             tx.commit();
         } catch (Exception e) {
